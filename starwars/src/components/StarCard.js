d258f549129d2e972f5330fdf352fb97b41ddd13
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-20 10:48:50 -0500

    I have styled the cards with syled-components

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -1,15 +1,34 @@
 import React from "react";
+import styled from "styled-components"
+
+const Container = styled.section`
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);   
+  margin: 0 auto;
+  margin-top: 2%;
+  margin-bottom: 2%;
+  width: 40%;
+  padding: 1%;
+  background-color: rgb(257,0,0);opacity:0.6;}
+`
+
+const Card = styled.h2`
+  color: White;
+`
+
+const Card1 = styled.h3`
+  color: black;
+`
 
 const StarCards = props => {
   
     return (
       
-        <div>
+        <Container>
           {/* <h1>Hello from StarCard!</h1> */}
-          <h2> {props.name} </h2>
-          <h3> {props.height} </h3>
-          <h3> {props.mass} </h3>
-        </div>
+          <Card> {props.name} </Card>
+          <Card1> {props.height} </Card1>
+          <Card1> {props.mass} </Card1>
+        </Container>
       
     )
     
