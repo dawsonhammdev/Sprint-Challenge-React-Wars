e1b2a3cee7f931b942d747b664ff973a62f1dde6
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-20 10:25:24 -0500

    I have set up my axios call and installed all of my dependencies for axios and styling.  I have gotten my data to display and set my data to state.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
 
-const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +9,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
-}
+  export default function App() {
+    const [stars, setStars] = useState([]);
 
-export default App;
+    useEffect(() => {
+      axios
+        .get("https://swapi.co/api/people/")
+        .then(res => {
+          console.log(res.data.results)
+          setStars(res.data.results)
+        })
+        .catch(error => {
+          console.log("The data was not returned", error);
+        });
+    }, []);
+
+    return (
+      <div className="App">
+        <h1 className="Header">React Wars</h1>
+      </div>
+    );
+  }
+
